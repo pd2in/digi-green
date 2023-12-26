596560ee5f1f1e959e88d3d81696fa9e0c7c5261
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-12-26 21:00:56 +0700

    fix styling of MinMaxPPM page

--- src/components/ColoredInput/index.js
+++ src/components/ColoredInput/index.js
@@ -1,20 +1,14 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import GlobalStyles from '../../styles/GlobalStyles';
 
-function ColoredInput({
-  label,
-  keyboardType,
-  placeholder,
-  value,
-  onChangeText,
-}) {
+function ColoredInput({ keyboardType, placeholder, value, onChangeText }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
       <TextInput
         style={styles.input}
         keyboardType={keyboardType}
         placeholder={placeholder}
+        placeholderTextColor={GlobalStyles.colors.primary}
         value={value}
         onChangeText={onChangeText}
       />
@@ -28,12 +22,9 @@ const styles = StyleSheet.create({
   container: {
     marginBottom: 16,
   },
-  label: {
-    fontFamily: 'Poppins-Bold',
-    marginBottom: 4,
-    color: GlobalStyles.colors.primary,
-  },
   input: {
+    fontFamily: 'Poppins-SemiBold',
+    color: GlobalStyles.colors.primary,
     backgroundColor: GlobalStyles.colors.inversePrimary,
     paddingHorizontal: 12,
     paddingVertical: 16,
