476f73dec0830d6888bc0f8a28fa219f1308f988
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-12-27 10:36:15 +0700

    created context API for global state management

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import {NavigationContainer} from "@react-navigation/native";
-import Router from "./src/router";
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/router';
+import HydroponicConfigContextProvider from './src/config/Context';
 
 export default function App() {
   return (
+    <HydroponicConfigContextProvider>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
+    </HydroponicConfigContextProvider>
   );
 }
-
